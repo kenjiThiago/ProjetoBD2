c3d3d0dd1efde4e3626a8898ccaf5960a48b3b71
Thiago Kenji <thiago.kenji123@gmail.com>
2025-06-20 05:09:55 -0300

    [back] A rota de cursos agora retorna a categoria.

--- backend/Database/classe_curso.py
+++ backend/Database/classe_curso.py
@@ -3,30 +3,31 @@ from Database.conector import DatabaseManager
 class Curso():
     def __init__(self, db_provider = DatabaseManager()) -> None:
         self.db = db_provider
-    
+
 
     def get_cursos(self, categoria: str = "", nivel: str = "", acesso: str = "", search: str = ""):
         query = """
-        SELECT 
+        SELECT
             c.nivel,
             ARRAY_AGG(DISTINCT h.nome) AS habilidades,
             c.nome,
             c.descricao,
+            c.categoria,
             p.nome AS nome_professor,
             FLOOR(c.duracao / 60) || 'h ' || (c.duracao % 60) || 'm' AS duracao_formatada,
             COUNT(DISTINCT e.cpf_aluno) AS numero_alunos_concluidos,
             c.acesso,
             TO_CHAR(c.data_lancamento, 'YYYY-MM-DD') AS data_lancamento
 
-        FROM 
+        FROM
             curso c
-        LEFT JOIN 
+        LEFT JOIN
             estuda e ON c.nome = e.nome_curso
-        LEFT JOIN 
+        LEFT JOIN
             habilidade_curso hc ON c.nome = hc.nome_curso
-        LEFT JOIN 
+        LEFT JOIN
             habilidade h ON hc.id_habilidade = h.id
-        LEFT JOIN 
+        LEFT JOIN
             ministra m ON c.nome = m.nome_curso
         LEFT JOIN
             professor p ON m.email_professor = p.email
@@ -55,9 +56,9 @@ class Curso():
             query += " WHERE " + " AND ".join(filtros)
 
         query += """
-        GROUP BY 
+        GROUP BY
             c.nome, c.descricao, c.duracao, c.nivel, c.data_lancamento, c.acesso, c.categoria, p.nome
-        ORDER BY 
+        ORDER BY
             c.data_lancamento DESC
         """
 
